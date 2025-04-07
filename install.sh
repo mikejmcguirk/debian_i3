fa9d55c780af34a96f55c9c08e6da54bc18ec2cb
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-04-06 22:55:43 -0400

    Updated Brave install with simplified method

--- install.sh
+++ install.sh
@@ -713,13 +713,7 @@ fi
 # Brave Browser
 ###############
 
-if [[ "$fresh_install" == true ]]; then
-    echo "Installing Brave..."
-
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt install -y brave-browser
-fi
+curl -fsS https://dl.brave.com/install.sh | sh
 
 # Post-install checks:
 # - Verify no errors when opening in terminal
